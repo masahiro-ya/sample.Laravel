fd26d6ad23cfe7584177dd2aea866014a2606dae
山下将大 <yamasyo-@yamashitamasahironoMacBook-Air.local>
2021-01-18 22:44:40 +0900

    first commit

---
+++ app/Mail/ResetPassword.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ResetPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $token;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject('パスワード再設定')
+            ->view('mail.password-reset');
+    }
+}
